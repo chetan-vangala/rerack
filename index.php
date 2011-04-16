faf6bb426cffe22bb2f39df966b5c0aa36c16072
Chris Butler <chris@platformthirteen.com>
2011-04-16 15:23:10 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<!-- chris butler did this - platformthirteen.com -->
+  <head>
+    <title>rerackapp.com</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href='http://fonts.googleapis.com/css?family=Kreon:regular,bold' rel='stylesheet' type='text/css'>
+    <link href="css/rerack.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="image_src" href="images/thumb.png" />
+    <script src="js/jquery.min.js" type="text/javascript"></script>
+    <script src="js/jquery.colorbox-min.js" type="text/javascript"></script>
+    <script type="text/javascript" >
+      $(document).ready(function(){
+			  $("a.colorbox").colorbox();
+			});
+		</script>
+  </head>
+  <body>
+    
+  </body>
+</html>
