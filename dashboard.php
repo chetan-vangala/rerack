a14ed791c1bbfe76b1d1a755ccb4903ea02c11de
Chris Butler <chris@platformthirteen.com>
2011-04-19 00:29:53 -0400

    true index page, redirects to dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,5 @@
+<?php require('shared/_head.php'); ?>
+<?php require 'shared/_header.php'; ?>
 <table>
   <tbody>
     <?php
@@ -31,12 +33,12 @@
   <tbody>
     <?php
       $teams = $t->find(array('queued=1', "house_id=$the_id"));
+    if(!empty($teams)){
       foreach($teams as $team){
-        if(!empty($teams)){
-          echo '<tr><td>';
-          echo $team->name . '</td><td>' . $team->player . '</td><td>' . $team->teammate . '</td><td>' . $team->wins . '</td></tr>';        
-          }
+        echo '<tr><td>';
+        echo $team->name . '</td><td>' . $team->player . '</td><td>' . $team->teammate . '</td><td>' . $team->wins . '</td></tr>';        
       }
+    }
     ?>
     </tbody>  
 </table>
--- shared/_code.php
+++
@@ -1,6 +0,0 @@
-<br />
-<form id="create-form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-  <label class="description" for="name">House Code:</label>
-  <input class="wide" type="text" name="house[code]" id="code" value="<?php echo double_ene_val($_POST,'house','code'); ?>" />
-  <input class="submit_btn" type="submit" name="submit" value="Play" />
-</form>
