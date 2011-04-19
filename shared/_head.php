ce67a5ae4d691d546c2c76e2852ab7229be8a496
Chris Butler <chris@platformthirteen.com>
2011-04-19 12:49:57 -0400

    prettier urls

--- shared/_head.php
+++ shared/_head.php
@@ -6,12 +6,12 @@
     <title>rerackapp.com</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <link href='http://fonts.googleapis.com/css?family=Kreon:regular,bold' rel='stylesheet' type='text/css'>
-    <link href="css/rerack.css" rel="stylesheet" type="text/css" />
-    <link rel="shortcut icon" href="images/favicon.png" />
-    <link rel="image_src" href="images/thumb.png" />
+    <link href="/css/rerack.css" rel="stylesheet" type="text/css" />
+    <link rel="shortcut icon" href="/images/favicon.png" />
+    <link rel="image_src" href="/images/thumb.png" />
     <!--script src="js/jquery.min.js" type="text/javascript"></script>
     <script src="js/jquery.colorbox-min.js" type="text/javascript"></script-->
-    <script src="js/jquery.mobile.min.js" type="text/javascript"></script>
+    <script src="/js/jquery.mobile.min.js" type="text/javascript"></script>
     <?php
       if(isset($_GET['house'])){
         $the_code = $_GET['house'];
