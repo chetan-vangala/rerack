b70ebca9b40d16df331f11affc1bf2eea21b0ea0
Chris Butler <chris@platformthirteen.com>
2011-04-19 14:55:00 -0400

    returning to single box

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
     ?>
     <br />
     <form id="code-input" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-      <input class="wide" type="text" name="code" id="code" size="5" value="<?php echo ene($_POST,'code'); ?>" />
-      <input class="submit_btn" type="hidden" name="submit" value="Play" />
+      <input type="text" name="code" id="code" size="5" value="<?php echo ene($_POST,'code'); ?>" />
+      <input id="code-submit" type="hidden" name="submit" value="go" />
     </form>
   </body>
 </html>
