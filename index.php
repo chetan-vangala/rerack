91437350322e03864ddd5855dab75c477ff5a831
Chris Butler <chris@platformthirteen.com>
2011-04-19 13:49:38 -0400

    styled login page with single input box

--- index.php
+++ index.php
@@ -1,34 +1,20 @@
 <?php require('shared/_head.php'); ?>
 </head>
-  <body>
+  <body id="login">
     <?php 
-      if(isset($_GET['house'])){
-        redirect_to('dashboard.php?house=' . $_GET['house']);
-      } else {
-        if(count($_POST) > 0){
-          $data = $_POST['code'];        
-          $errors = array();
-          if(!empty($data)){
-            $errors[] = 'All fields must be complete.';
-          }
-          redirect_to('dashboard.php?house=' . $data);
-          /*$house = new House();
-          $h = $house->find('code=' . $data);
-          if(!empty($h)){
-            
-            return;
-          } else {
-            $errors[] = "Unable to find house.";
-          }*/
-          print_errors($errors); 
+      if(count($_POST) > 0){
+        $data = $_POST['code'];
+        if(empty($_POST['code'])){
+          print_errors('All fields must be complete.');
+        } else {      
+        redirect_to('dashboard.php?house=' . $data);
         }
       }
     ?>
     <br />
-    <form id="create-form" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-      <label class="description" for="name">House Code:</label>
-      <input class="wide" type="text" name="code" id="code" value="<?php echo ene($_POST,'code'); ?>" />
-      <input class="submit_btn" type="submit" name="submit" value="Play" />
+    <form id="code-input" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+      <input class="wide" type="text" name="code" id="code" size="5" value="<?php echo ene($_POST,'code'); ?>" />
+      <input class="submit_btn" type="hidden" name="submit" value="Play" />
     </form>
   </body>
 </html>
