13fcf8463089bcae2a9254233daf2922f18e8cc3
Chris Butler <chris@platformthirteen.com>
2011-04-20 18:52:30 -0400

    removing echos

--- shared/classes/table_class.php
+++ shared/classes/table_class.php
@@ -10,12 +10,10 @@ class Table extends Model{
         $t_del = $team->find(array("id=" . $tab[0]->team_id));
           
         $tab[0]->team_id = $team->id;
-        echo "<br />opponent:  " . $team->id . "  " .  $t_del[0]->id . "  " . $tab[0]->opponent_id;
       } elseif($team->id == $tab[0]->team_id){ //incumbent
         $team->wins += 1;
         $team->save();
-        $t_del = $team->find(array("id=" . $tab[0]->opponent_id));
-        echo "<br />incumbent:  " . $team->id . "  " .  $tab[0]->opponent_id;  
+        $t_del = $team->find(array("id=" . $tab[0]->opponent_id)); 
       }
       $team->highscore($house);
       $tmp = $team->find(array("house_id=$house->id", "queued=1"));
@@ -34,20 +32,17 @@ class Table extends Model{
     $e_found = empty($e_tables) ? 0 : count($e_tables);
     
     if($p < ($numTables * 2)){
-      echo "<br />less:  $p $team->id $numTables"; 
       $team->queued = 0;
       if($house->left != 0) $house->left -= 1;
       $team->save();
       $house->save();
       if(($found == 0) || ($found < $numTables && !($p%2) && $e_found == 0)){
-        echo "<br />create:  $found $team->id $numTables $e_found";
         $temp = new Table();
         $temp->house_id = $house->id;
         $temp->team_id = $team->id;
         $temp->save();
         return 0;
       } elseif($p%2 && ($found <= $numTables) && $e_found != 0){
-        echo "<br />fill:  $found $team->id $numTables $e_found";
         $e_tables[0]->opponent_id = $team->id;
         $e_tables[0]->update();
         return 1;
