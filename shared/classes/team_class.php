b21b36f891917aad89e05afd51004ed88426ecc4
Chris Butler <chris@platformthirteen.com>
2011-04-19 23:59:07 -0400

    set first user to high score

--- shared/classes/team_class.php
+++ shared/classes/team_class.php
@@ -9,15 +9,16 @@ class Team extends Model{
     $tab = new Table();
     $h = new House();
     $playing = count($this->find(array('queued=0', "house_id=$this->house_id")));
-    $house = $h->find($this->house_id);
+    $house = $h->find(array("id=$this->house_id"));
     $numTables = $house[0]->tables;
     if($playing < ($numTables * 2)){
       $this->queued = 0;
       $this->save();
       $i = $tab->tableUpdate($this->house_id, $this->id, $numTables, $playing);
       if(!$i){
+        echo "$this->id,  $this->house_id,  " . $house[0]->id . ",  ";
         $house[0]->top_team_id = $this->id;
-        $house[0]->save();
+        echo $house[0]->update();
       }
     } else {
       $house[0]->left += 1;
