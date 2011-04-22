315887c317a07a85d5ec41bc6b15788bab29c07b
Chris Butler <chris@platformthirteen.com>
2011-04-22 19:31:48 -0400

    fades

--- js/rerack.js
+++ js/rerack.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   
   window.scrollTo(0,1);
   
+  $("#wrapper").css("display", "none");
+  $("#wrapper").fadeIn(1000);
+    
 });
--- new.php
+++ new.php
@@ -1,19 +1,13 @@
 <?php require('shared/_head.php');
 
 $house = new House();
-//Has the form been submitted?
 if(count($_POST) > 0){
   $data = $_POST['house'];
-
   $errors = array();
-
-  //Verify the fields are filled out
   if(!ene($data,array('name','tables'))){
     $errors[] = 'All fields must be complete.';
   }
-  //If there are no error thus far, continue creating the survey
   if(empty($errors)){
-    //Update all the attributes we can in the array.
     $house->update_attributes($data);
     if($house->create()){
       redirect_to('index.php');
@@ -22,13 +16,9 @@ if(count($_POST) > 0){
       $errors[] = "Unable to create house.";
     }
   }
-  //Display any error messages we've encountered.
-  if(!empty($errors)){
     print_errors($errors);
   }
-  
 } else {
-  $house->name = "";
 }
 ?>
 </head>
