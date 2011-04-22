3c7fb051daa4f5a46f0b2d75e1a9339595f5e964
Chris Butler <chris@platformthirteen.com>
2011-04-22 13:31:21 -0400

    only scale 50%

--- shared/_head.php
+++ shared/_head.php
@@ -5,7 +5,7 @@
   <head>
     <title>rerackapp.com</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=640; initial-scale=0; maximum-scale=1.0; user-scalable=1;">
+    <meta name="viewport" content="width=640; initial-scale=0; maximum-scale=.5; user-scalable=1;">
     <link href='http://fonts.googleapis.com/css?family=Kreon:light,regular,bold' rel='stylesheet' type='text/css'>
     <link href="/css/rerack.css" rel="stylesheet" type="text/css" />
     <link rel="shortcut icon" href="/images/favicon.ico" />
