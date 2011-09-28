21d704ad358993f29272f145a679d78b0071c149
Chris Butler <chris@platformthirteen.com>
2011-09-27 20:28:54 -0400

    add link to bypass beta signup

--- index.php
+++ index.php
@@ -18,11 +18,7 @@
     ?>
     <img src="/images/rr-logo.gif" id="top-logo" />
     <div id="code">
-      <form id="code-input" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
-        <input size="5" maxlength="5" type="text" class="clean" name="code" id="code" spellcheck="false" value="<?php echo ene_val($_POST,'code'); ?>" />
-        <input class="inline-submit" type="submit" value="go" />
-      </form>
-      <div id="errors"></div>
+      <a href="/code.php">I already have a house code</a>
     </div>
     <div id="signup" data-role="content">
       <iframe id='prefinery_iframe_inline' allowTransparency='true' width='100%' height='600' scrolling='no' frameborder='0' src="http://platformthirteen.prefinery.com/betas/2361/testers/new?display=inline"></iframe>
