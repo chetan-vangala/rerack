8588b498706b3e79cdce87d81e1d5db708bc0e12
Chris Butler <chris@platformthirteen.com>
2013-08-25 13:40:03 -0400

    update db config

--- shared/config.inc.php
+++ shared/config.inc.php
@@ -2,10 +2,11 @@
 error_reporting(E_ALL);
 define('KEY', '23423'); //Used to encrypt/decrypt ids
 
-define('DB_USERNAME', 'rerack'); //Username
-define('DB_PASSWORD', 'rerack'); //Password
-define('DB_NAME', 'rerack'); //Database name
-define('DB_HOST', 'localhost'); //Database Server
+$url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+define('DB_USERNAME', $url["user"]); //Username
+define('DB_PASSWORD', $url["pass"]); //Password
+define('DB_NAME', substr($url["path"],1)); //Database name
+define('DB_HOST', $url["host"]); //Database Server
 
 define('TAV','2010-04-01'); // twilio api version
 define('TSID', 'ACb421c4f6d7aa53a5f87cc9d11e51aaa0'); //sid
