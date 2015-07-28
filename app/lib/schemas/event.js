1a4f06eee6f53276964893ad8c9f6a9d93068a7d
Chetan Vangala <chetan.vangala@gmail.com>
2015-07-27 21:08:32 -0400

    updated event schema

--- app/lib/schemas/event.js
+++ app/lib/schemas/event.js
@@ -1,1 +1,20 @@
-event.js
+Schema.event = new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name",
+    max: 100
+  },
+  date: {
+    type: Date
+  },
+  locationID: {
+    type: Number
+  },
+  visibility: {
+    type: String,
+    allowedValues: ['public', 'unlisted', 'private']
+  },
+  tables:{
+    type: [Number]
+  }
+});
