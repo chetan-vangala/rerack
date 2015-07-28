d74cf10e381bda395a1b46123c74e6b13bf69447
Chris Butler <chris@platformthirteen.com>
2015-07-28 03:55:06 -0400

    [#97454840] admin user bootstrapping

--- app/server/bootstrap.js
+++ app/server/bootstrap.js
@@ -1,2 +1,13 @@
 Meteor.startup(function () {
+  if (Meteor.users.find().count() === 0) {
+     var u = Accounts.createUser({
+      'email' : 'admin@rerack.it',
+      'password' : 'rerackit',
+      'username': 'rerack',
+      'profile': {
+        'name': 'Rerack Admin'
+      }
+    });
+    console.log('admin user created', u);
+  }
 });
